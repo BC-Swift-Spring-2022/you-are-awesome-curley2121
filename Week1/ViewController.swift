c6b8d4fbc8a26ad58e3b92cb5791bbcb494eb07b
Sean Curley <curleysc@bc.edu>
2022-01-24 17:46:53 -0500

    Initial Commit

---
+++ Week1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week1
+//
+//  Created by Sean C. on 1/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
