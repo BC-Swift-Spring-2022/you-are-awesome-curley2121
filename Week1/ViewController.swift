a3e68b7b3b88ddc6f9e87a543a57476b7bdd02ff
Sean Curley <curleysc@bc.edu>
2022-01-24 18:40:13 -0500

    first

--- Week1/ViewController.swift
+++ Week1/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Loaded")
     }
 
-
+    @IBAction func sayItButtonPressed(_ sender: UIButton) {
+        print("Say it button was pressed")
+        messageLabel.text = "You are awesome!"
+    }
+    
 }
 
